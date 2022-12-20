5af2d2eed5bb54abce0db6491fe4c7ae4c4e9249
Artur Kulaev <artur.korol1488@yandex.ru>
2022-12-20 10:31:38 +0300

    fix slider in ff

--- src/blocks/proposal/proposal.js
+++ src/blocks/proposal/proposal.js
@@ -1,71 +1,70 @@
-const 	sliderTrack = document.querySelector('._proposal__slider-track'),
-		btnNext = document.querySelector('.proposal__button_right'),
-		btnPrev = document.querySelector('.proposal__button_left'),
-		dots = document.querySelectorAll('._proposal__dot');
-		images = document.querySelectorAll('._proposal__slider-track img');
-
-let 	position = 0,
-		dotIndex = 0,
-		itemWidth = document.querySelector('._proposal__slider-item').offsetWidth,
-		sliderTrackWidth = document.querySelector('._proposal__slider-track').offsetWidth;
-
-console.log(sliderTrackWidth)
-
-// FUNCTIONS
-
-const nextSlide = () =>
+o2.slider =
 {
-	if ( position < (dots.length - 1) * itemWidth)
-	{
-		position += itemWidth
-		dotIndex++
-	}
-	else
-	{
-		dotIndex = 0
-		position = 0
-	}
-	sliderTrack.style.left = -position + 'px'
-	thisSlide(dotIndex)
-};
+	sliderTrack: document.querySelector('._proposal__slider-track'),
+	btnNext: document.querySelector('.proposal__button_right'),
+	btnPrev: document.querySelector('.proposal__button_left'),
+	dots: document.querySelectorAll('._proposal__dot'),
 
-const prevSlide = () =>
-{
-	if ( position > 0 )
-	{
-		position -= itemWidth
-		dotIndex--
-	}
-	else
-	{
-		dotIndex = dots.length - 1
-		position = (dots.length - 1) * itemWidth
-	}
-	sliderTrack.style.left = -position + 'px'
-	thisSlide(dotIndex)
-};
+	position: 0,
+	dotIndex: 0,
+	itemWidth: null,
 
-const thisSlide = (index) =>
-{
-	for (let dot of dots)
+	init()
 	{
-		dot.classList.remove('proposal__dot_active')
+		setTimeout(() => {
+			this.itemWidth = document.querySelector('._proposal__slider-item').offsetWidth;
+			console.log(this.itemWidth)
+		}, 300)
+	},
+	dotsInit: _this => o2.slider.dots.forEach( (dot, index) =>
+	{
+		if(_this === dot)
+		{
+			console.log(index)
+			o2.slider.position = o2.slider.itemWidth * index;
+			o2.slider.sliderTrack.style.left = -o2.slider.position + 'px';
+			o2.slider.dotIndex = index;
+			o2.slider.currentSlide(o2.slider.dotIndex)
+			return;
+		}
+	}),
+	nextSlide()
+	{
+		if ( this.position < (this.dots.length - 1) * this.itemWidth)
+		{
+			this.position += this.itemWidth
+			this.dotIndex++
+		}
+		else
+		{
+			this.dotIndex = 0
+			this.position = 0
+		}
+
+		this.sliderTrack.style.left = -this.position + 'px';
+		this.currentSlide(this.dotIndex)
+	},
+	prevSlide()
+	{
+		if ( this.position > 0 )
+		{
+			this.position -= this.itemWidth
+			this.dotIndex--
+		}
+		else
+		{
+			this.dotIndex = this.dots.length - 1
+			this.position = (this.dots.length - 1) * this.itemWidth
+		}
+		this.sliderTrack.style.left = -this.position + 'px'
+		this.currentSlide(this.dotIndex)
+	},
+	currentSlide(index)
+	{
+		for (let dot of this.dots)
+		{
+			dot.classList.remove('proposal__dot_active')
+		}
+		this.dots[index].classList.add('proposal__dot_active')
 	}
-	dots[index].classList.add('proposal__dot_active')
 }
-
-// EVENTLISTENERS
-
-btnNext.addEventListener( 'click', nextSlide );
-btnPrev.addEventListener( 'click', prevSlide );
-
-dots.forEach( ( dot, index) =>
-	{
-	dot.addEventListener('click', () =>
-	{
-		position = itemWidth * index
-		sliderTrack.style.left = -position + 'px';
-		dotIndex = index
-		thisSlide(dotIndex)
-	})
-})
